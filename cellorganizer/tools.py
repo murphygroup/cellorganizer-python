a239fc562f8030fe54f00ee89c416f7ed4a98f0a
Ivan E. Cao-Berg <icaoberg@andrew.cmu.edu>
2018-06-29 10:44:07 -0400

    Basic untested commit

---
+++ cellorganizer/tools.py
@@ -0,0 +1,14 @@
+def img2slml():
+   return None
+
+def slml2img( ):
+   return None
+
+def slml2info( ):
+   return None
+
+def slml2slml( ):
+   return None
+
+def slml2report( ):
+   return None
---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+setup(name='cellorganizer',
+      version='2.7.2',
+      description='',
+      author='CellOrganizer Team',
+      author_email='icaoberg@cmu.edu',
+      maintainer='Robert F. Murphy',
+      maintainer_email='murphy@cmu.edu',
+      url='http://www.cellorganizer.org',
+      packages = ['cellorganizer'])
+
