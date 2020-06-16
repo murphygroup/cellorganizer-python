2d86495ec9400039e0c0bd980a9b3e4f8511a9e3
Ivan E. Cao-Berg <icaoberg@cmu.edu>
2020-06-15 22:46:21 -0400

    Updated setup file to CellOrganizer v2.9.0

--- setup.py
+++ setup.py
@@ -1,12 +1,11 @@
 from distutils.core import setup
 setup(name='cellorganizer',
-      version='2.8.0',
-      description='',
+      version='2.0.0',
+      description='Python wrapper for CellOrganizer binaries',
       author='CellOrganizer Team',
-      author_email='icaoberg@cmu.edu',
-      maintainer='Robert F. Murphy',
-      maintainer_email='murphy@cmu.edu',
+      author_email='cellorganizer-dev@compbio.cmu.edu',
+      maintainer='CellOrganizer Team',
+      maintainer_email='cellorganizer-dev@compbio.cmu.edu',
       url='http://www.cellorganizer.org',
       packages = ['cellorganizer'],
       )
-
