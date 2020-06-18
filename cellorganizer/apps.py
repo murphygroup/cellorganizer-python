bc513413564d6097e09c8c91213401301cfa5755
icaoberg <icaoberg@andrew.cmu.edu>
2020-06-18 14:21:55 -0400

    Update apps.py

--- cellorganizer/apps.py
+++ cellorganizer/apps.py
@@ -8,32 +8,13 @@ import urllib.request
 ################################################################################
 # Public Methods
 ################################################################################
-def img2slml(dim, dna, cell, protein, options):
-    '''
-    Trains a generative model of protein subcellular location from a
-    collection of microscope images and saves the model as an SLML instance.
-
-    An SLML model consists of four components,
-    1) a (optional) documentation component
-    2) a nuclear pattern model,
-    3) a cell pattern model and,
-    4) a protein pattern model.
-
-    List Of Input Arguments     Descriptions
-    -----------------------     ------------
-    dimensionality              2D/3D
-    dnaImagesDirectoryPath      DNA images collection directory
-    cellImagesDirectoryPath     Cell images collection directory
-    proteinImagesDirectoryPath  Protein images collection directory
-    options                     Options structure
+def img2shapespace( dna, cell, options ):
+    '''                   
     '''
 
     txtfilename = "input.txt"
     __options2txt(options,txtfilename)
     f = open(txtfilename,"a")
-
-    text = "dimensionality = '" + dim +"';\n"
-    f.write(text)
 
     f.write("dnaImagesDirectoryPath = {")
     text = ""
@@ -52,17 +33,7 @@ def img2slml(dim, dna, cell, protein, options):
     text = text[:-1]
     text = text+"};\n"
     f.write(text)
-
-    text = ""
-    f.write("proteinImagesDirectoryPath = {")
-
-    for name in protein:
-        text = text + "'" + name + "',"
-
-    text = text[:-1]
-    text = text+"};\n"
-    f.write(text)
     f.close()
 
-    os.system("img2slml input.txt;rm input.txt")
+    os.system("img2shapespace input.txt;rm input.txt")
     return None
