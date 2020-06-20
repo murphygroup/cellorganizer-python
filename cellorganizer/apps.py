5615e26a07b5e9abce721b58a06fea076f5c4445
Ivan E. Cao-Berg <icaoberg@cmu.edu>
2020-06-20 17:24:37 -0400

    Updated apps

--- cellorganizer/apps.py
+++ cellorganizer/apps.py
@@ -4,12 +4,13 @@ import scipy.io
 import matplotlib.pyplot as plt
 from pathlib import Path
 import urllib.request
+import cellorganizer.tools
 
 ################################################################################
 # Public Methods
 ################################################################################
-def img2shapespace( dna, cell, options ):
-    '''                   
+def img2shapespace( nuc, cellm, options ):
+    '''
     '''
 
     txtfilename = "input.txt"
@@ -22,7 +23,7 @@ def img2shapespace( dna, cell, options ):
     # dnaImageDirectoryPath = {};
     f.write("dnaImagesDirectoryPath = {")
     text = ""
-    for name in dna:
+    for name in nuc:
         text = text + "'" + name + "',"
 
     text = text[:-1]
@@ -32,7 +33,7 @@ def img2shapespace( dna, cell, options ):
     #soham do the same thing for cell
     f.write("cellImagesDirectoryPath = {")
     text = ""
-    for name in cell:
+    for name in cellm:
         text = text + "'" + name + "',"
 
     text = text[:-1]
@@ -42,3 +43,67 @@ def img2shapespace( dna, cell, options ):
 
     os.system("img2shapespace input.txt;rm input.txt")
     return None
+
+################################################################################
+#Private Methods
+################################################################################
+def __options2txt(options,filename):
+    if os.path.exists(filename):
+        os.system('rm '+filename)
+    f = open(filename,"w")
+    keys = list(options.keys())
+    keys.sort()
+    for key in keys:
+        if isinstance(options[key],str):
+            if 'pwd' in options[key]:
+                if options[key] == 'pwd':
+                    text = 'options.'+key+' = '+ options[key]+";\n"
+                else:
+                    text =  'options.'+key+' = '+"["+ options[key]+"];\n"
+            # if value is list or matrix
+            elif options[key] == 'date':
+                text = 'options.'+key+' = '+ options[key]+";\n"
+            elif '[' in options[key]:
+                text = 'options.'+key+' = '+ options[key]+";\n"
+            # if value is a function
+            elif '(' in options[key]:
+                text = 'options.'+key+' = '+ options[key]+";\n"
+            elif '{' in options[key]:
+                text = 'options.'+key+' = '+ options[key]+";\n"
+            else:
+                text = 'options.'+key+' = '+ "'"+options[key]+"';\n"
+        elif isinstance(options[key],bool):
+            if options[key]:
+                text = 'options.'+key+' = '+ 'true;\n'
+            else:
+                text = 'options.'+key+' = '+ 'false;\n'
+        # if value is list
+        elif isinstance(options[key],list):
+            if len(options[key])<1:
+                text = 'options.'+key+' = [];\n'
+            else:
+                if key == "masks":
+                    text = 'options.'+key+' = {'
+                else:
+                    text = 'options.'+key+' = ['
+                for element in options[key]:
+                    # if element in list is str
+                    if isinstance(element,str):
+                        text = text + "'" + element + "',"
+                    # if element in list is float, keep three decimal places
+                    elif isinstance(element,float):
+                        text = text + str('%.3f' % element) + ","
+                    else:
+                        text = text + str(element) + ","
+                text = text[:-1]
+                if key == "masks":
+                    text = text+"};\n"
+                else:
+                    text = text+"];\n"
+
+        elif isinstance(options[key],float):
+            text = 'options.'+key+ ' = ' +str('%.3f' %  options[key])+';\n'
+        else:
+            text = 'options.'+key+ ' = ' +str(options[key])+';\n'
+        f.write(text)
+    f.close()
