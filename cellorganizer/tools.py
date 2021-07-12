0b2a29cb6e3a2f6b4a33e375f578477dbb207a34
Ted Zhang <tedz@andrew.cmu.edu>
2021-07-12 16:05:30 -0400

    update image pulling script

--- cellorganizer/tools.py
+++ cellorganizer/tools.py
@@ -282,7 +282,8 @@ def get_image_collection():
 
     # url = 'http://www.cellorganizer.org/Downloads/latest/docker/images.zip'
     url = 'https://drive.google.com/file/d/1v4RN7feITmYRiMyl7gSb-OhNABSlSEfC/view?usp=sharing'
-    target_path = "/home/murphylab/cellorganizer/local/images.zip"
+    target_path = "/home/murphylab/cellorganizer/local/mmbios-images.zip"
+    print('Retrieving ' + url)
     if __does_file_exist(url):
         if not os.path.exists('/home/murphylab/cellorganizer/local/images/'):
             print('Creating directory /home/murphylab/cellorganizer/local/images/')
