ed791331d4f92e4a9d22ab4387c6182f67ab13d3
Ted Zhang <tedz@andrew.cmu.edu>
2021-07-12 22:36:03 -0400

    new url for images

--- cellorganizer/tools.py
+++ cellorganizer/tools.py
@@ -281,7 +281,8 @@ def get_image_collection():
     '''
 
     # url = 'http://www.cellorganizer.org/Downloads/latest/docker/images.zip'
-    url = 'https://drive.google.com/u/0/uc?export=download&confirm=8zsc&id=1v4RN7feITmYRiMyl7gSb-OhNABSlSEfC'
+    url = 'http://www.andrew.cmu.edu/~icaoberg/mmbios-images.zip'
+
     target_path = "/home/murphylab/cellorganizer/local/mmbios-images.zip"
     print('Retrieving ' + url)
     if __does_file_exist(url):
